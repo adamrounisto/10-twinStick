0d26edb3f084e78cf70710f242fc7fc7e361fb8d
LAPTOP-A08KA1L7\Adam <adamrounisto@gmail.com>
2018-07-23 01:17:42 -0400

    added something

---
+++ Assets/FunScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("dick dickson");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
